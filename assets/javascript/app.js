68848259cf9b70480610b3e00cc6d072f8c85a08
chylinski.t <chylinski.t@outlook.com>
2019-11-15 19:40:28 -0500

    created my api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,27 @@
 var topics = ['arsenal', 'robert lewnadowski','champions league','world cup'];
 
 
+for(i=0;i < topics.length;i++){
+
+ var topicBtn = "<button>"
+
+ // i will add a class to this button to make it style 
+
+    // topicBtn.addClass('primiary-button');
+
+//i can also add an attribute 
+    //topicBtn.attr("main-cat",topics[i])
+
+//finally add to the html page by appending to a div 
+   // $("#main-section").append(topicBtn)
+
+}
+
+
+
+
+
+//create an arrary named topics 
+//loop through arrary and create a button for each topic 
+//create an account to get an api key
+//import my api key - 2M3P6h2192Qg2v002nmFmDC48LvprBab 
