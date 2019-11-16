ba21b31a128fb910b71c78e2aee9abe72c323c56
chylinski.t <chylinski.t@outlook.com>
2019-11-15 21:35:04 -0500

    creat my ajax call, my inital arrary had to be adjusted

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var topics = ['arsenal', 'robert lewnadowski','champions league','world cup'];
+var topics = ['apples','piers','oranges','world cup'];
 
 
 for(i=0;i < topics.length;i++){
@@ -6,18 +6,43 @@ for(i=0;i < topics.length;i++){
  var topicBtn = "<button>" + topics[i] + "</button>"
 
 
- // i will add a class to this button to make it style 
+    // i will add a class to this button to make it style 
 
- //   topicBtn.addClass('primiary-button');
+    //topicBtn.addClass("btn btn-primary");
 
-//i can also add an attribute 
-    //topicBtn.attr("main-cat",topics[i])
+    //i can also add an attribute 
+        //topicBtn.attr("button",topics[i])
 
-//finally add to the html page by appending to a div 
+    //finally add to the html page by appending to a div 
    $("#main-section").append(topicBtn)
 
 }
 
+
+$("button").on("click", function(){
+
+    //capture value of my selected button
+    var topicCat = $(this).text().trim();
+
+    //construct a queryURL 
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topicCat + "&api_key=2M3P6h2192Qg2v002nmFmDC48LvprBab&limit=10";
+
+    //create ajax call
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+
+    })
+
+    //Get response --- then is a promise that is used to set data once its completed 
+    .then(function(response){
+        console.log('testing my api call ' + queryURL);
+
+        //note to self remember to add ---   &api_key=
+
+    })
+
+})
 
 
 
@@ -26,3 +51,5 @@ for(i=0;i < topics.length;i++){
 //loop through arrary and create a button for each topic 
 //create an account to get an api key
 //import my api key - 2M3P6h2192Qg2v002nmFmDC48LvprBab 
+// on a click we want to trigger the api
+//create a variable within the click to capture the button value you want to access
