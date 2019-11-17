c61879351a28b0ca097ac817e9924a0c4d481b4a
chylinski.t <chylinski.t@outlook.com>
2019-11-16 22:00:00 -0500

    added input functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,24 @@
-var topics = ['soccer','football','basketball','tennis'];
+var topics = ["soccer", "football", "basketball", "tennis"];
+
+createNav()
 
 
-for(i=0;i < topics.length;i++){
+function createNav() {
 
-     var topicBtn = $("<button>");
+    $("#add-topic").on("click", function() {
+        var addTopic = $("#topic-input")
+          .val()
+          .trim()
+          .toLowerCase();
+    
+        topics.push(addTopic);
+        createNav();
+        console.log("new arrary " + topics);
+      });
 
+
+  for (i = 0; i < topics.length; i++) {
+    var topicBtn = $("<button>");
 
     // adding two classes to my buttons
     topicBtn.addClass("btn btn-info");
@@ -12,77 +26,78 @@ for(i=0;i < topics.length;i++){
     // adding an attribute to the button
     topicBtn.attr("image-data", topicBtn[i]);
 
-    //add text to button 
+    //add text to button
     topicBtn.text(topics[i].toUpperCase());
-   
-    //finally add to the html page by appending to an id 
-   $("#nav-item").append(topicBtn)
 
+    //finally add to the html page by appending to an id
+    $("#nav-item").append(topicBtn);
+  }
 }
 
-$("button").on("click", function(){
+$("button").on("click", function() {
+  //capture value of my selected button
+  var topicCat = $(this)
+    .text()
+    .trim();
 
-    //capture value of my selected button
-    var topicCat = $(this).text().trim();
+  //construct a queryURL
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    topicCat +
+    "&api_key=2M3P6h2192Qg2v002nmFmDC48LvprBab&limit=10";
 
-    //construct a queryURL 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topicCat + "&api_key=2M3P6h2192Qg2v002nmFmDC48LvprBab&limit=10";
+  //create ajax call
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
 
-    //create ajax call
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
+    //Get response --- then is a promise that is used to set data once its completed
+    .then(function(response) {
+      console.log("my query url " + queryURL);
 
-    //Get response --- then is a promise that is used to set data once its completed 
-    .then(function(response){
-        console.log('my query url ' + queryURL);
+      console.log("testing my api call ", response);
 
-        console.log('testing my api call ' , response);
+      //need to define the results
+      var results = response.data;
 
-        //need to define the results
-        var results = response.data;
+      console.log("get results  ", results);
+      console.log("get first rating ", results[0].rating);
+      //note to self remember to add ---   &api_key=
 
-        console.log('get results  ' , results)
-        console.log('get first rating ' , results[0].rating)
-        //note to self remember to add ---   &api_key=
+      for (i = 0; i < results.length; i++) {
+        var divHolder = $("<div>");
 
-        for(i=0; i < results.length; i++){
+        divHolder.addClass("inline");
 
-            var divHolder = $('<div>');
+        //create img holder for each gif
+        var gifyImgHolder = $("<img>");
+        gifyImgHolder.attr("src", results[i].images.fixed_height_still.url);
 
-            divHolder.addClass("inline");
+        var gifyRating = $("<p>").text(
+          "Rating: " + results[i].rating.toUpperCase()
+        );
 
-            //create img holder for each gif
-            var gifyImgHolder = $('<img>');
-            gifyImgHolder.attr("src", results[i].images.fixed_height_still.url);
+        //attach both image and rating to my new div
+        divHolder.append(gifyImgHolder);
+        divHolder.prepend(gifyRating);
 
-            var gifyRating = $('<p>').text('Rating: ' + results[i].rating.toUpperCase())
+        //prepend/display my new div to an already existing html div
+        $("#main-section").append(divHolder);
+      }
+    });
+});
 
-            //attach both image and rating to my new div 
-            divHolder.append(gifyImgHolder);
-            divHolder.prepend(gifyRating)
+/*function addNewTopic() {
+  $("#add-topic").on("click", function() {
+    var addTopic = $("#topic-input")
+      .val()
+      .trim()
+      .toLowerCase();
 
-            //prepend/display my new div to an already existing html div 
-            $("#main-section").append(divHolder);
-        }
-        // NEXT STEPS 
-            /*
-                1. add 10 images for each topic
-                2. add rating under each image
-                3. if clicked the image should animate 
-             */
-
-    })
-
-})
-
-
-
-
-//create an arrary named topics 
-//loop through arrary and create a button for each topic 
-//create an account to get an api key
-//import my api key - 2M3P6h2192Qg2v002nmFmDC48LvprBab 
-// on a click we want to trigger the api
-//create a variable within the click to capture the button value you want to access
+    topics.push(addTopic);
+    createNav();
+    console.log("new arrary " + topics);
+  });
+}
+*/
